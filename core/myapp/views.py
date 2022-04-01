f02555962a912103f098e6fa690ea52759f854b5
Franco Miguez <franco.miguez98@gmail.com>
2022-04-01 15:13:08 -0300

    end of sesion 27 the master in python

--- core/myapp/views.py
+++ core/myapp/views.py
@@ -1,31 +1,20 @@
 from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
-layout = """<h1>site web with Django | Franco Miguez</h1>
-<br>
-<ul>
-    <li><a href=''>home</a></li>
-    <li><a href='/hello-word'>hello word</a></li>
-    <li><a href='/page'>page</a></li>
-    <li><a href='/contact'>contact</a></li>
-</ul>
-<br>
+layout = """
 
 """
 
 def index(request):
-    return HttpResponse(layout+"""<h2>Home</h2>
-    <p>Welcome</p>
-    """)
+    return render(request,'index.html')
 
 def hello_word(request):
-    return HttpResponse(layout+'<h2>Hello Word whit Django!!!</h2>')
+    return render(request, 'hello_word.html')
 
 def page(request, number=0):
     if number==5:
         return redirect('contact',name='franco')
-    return HttpResponse(layout+"""<h2>My page</h2>
-    <p>the Franco Miguez</p>""")
+    return render(request, 'page.html')
 
 def contact(request, name=""):
-    return HttpResponse(layout+f"""<h2>Contacto {name}</h2>""")
+    return render(request, 'contact.html')
